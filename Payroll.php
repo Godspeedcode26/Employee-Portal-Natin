80afaf50916f9cdbb6a9b67cb4adc1745d1846db
Godspeedcode26 <100329965+Godspeedcode26@users.noreply.github.com>
2022-03-02 20:45:46 -0600

    Add files via upload

---
+++ Payroll.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<htal lang="en" dir="ltr">
+  <head>
+    <neta charset="utf-8">
+    <title>Employee Portal</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  </head>
+  <body>
+     <!--header start-->
+  <header>
+  <div class="left_area">
+    <h3>Employee <span>Portal</span></h3>
+  </div>
+  
+</header>
+ <!--header end-->
+
+  <!--sidebar start-->
+<div class="sidebar">
+<center>
+<img src="images/natin.png" alt="" width="130px" height="150px" class="profile-img">
+
+</center>
+<a href="index.php"><i class="fa-solid fa-user"></i><span>Profile</span></a>
+<a href="LeaveOfAbsence.php"><i class="fa-solid fa-house-chimney"></i><span>Leave of Absence</span></a>
+<a href="Reporting.php"><i class="fa-solid fa-chart-column"></i><span>Reporting</span></a>
+<a href="Payroll.php"><i class="fa-solid fa-money-check-dollar"></i><span>Payroll</span></a>
+<a href="settings.php"><i class="fa-solid fa-gear"></i><span>Settings</span></a>
+<a href="#"><i class="fa-solid fa-right-from-bracket"></i><span>Log Out</span></a>
+</div>
+<!--sidebar end-->
+  </body>
+</htal>
